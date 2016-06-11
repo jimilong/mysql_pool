a65ce68da20331d9eecfe502363ae0719e6487f8
longmsdu <longmsdu@163.com>
2016-06-11 21:36:47 +0800

    支持事务

--- pool.php
+++ pool.php
@@ -79,15 +79,24 @@ $serv->start();*/
 $serv = new swoole_server("127.0.0.1", 9501);
 
 //设置异步任务的工作进程数量
-$serv->set(array('worker_num' => 3, 'task_worker_num' => 4));
+$serv->set(array('worker_num' => 4, 'task_worker_num' => 2));
 
 $serv->on('receive', function($serv, $fd, $from_id, $data) {
-    //投递异步任务
     $mem = new Memcached();
     //链接一台memcahe服务
     $mem->addServer('127.0.0.1','11211');
-    $mem->add('fdd', $fd);
-    $task_id = $serv->task($data);
+    $mem->set('fdd', $fd);
+    $mem->set('worker_id', $serv->worker_id);
+    //投递异步任务
+    $data = unserialize($data);
+
+    //$task_id = $serv->task($data, 5);
+    if (isset($data['worker_id'])) {
+        $task_id = $serv->task($data, $data['worker_id']);
+    } else {
+        $task_id = $serv->task($data);
+    }
+
     echo "Dispath AsyncTask: id=$task_id\n";
 });
 
@@ -96,25 +105,47 @@ $serv->on('task', function ($serv, $task_id, $from_id, $data) {
     static $link = null;
     if ($link == null)
     {
-        $link = new PDO('mysql:host=127.0.0.1;dbname=test', 'root', 123456);;
+        $link = new PDO('mysql:host=127.0.0.1;dbname=test', 'root', 123456);
         if (!$link)
         {
             $link = null;
             return array("data" => '', 'error' => "connect database failed.");
         }
     }
-    $result = $link->query($data);
-    if (!$result)
-    {
-        return array("data" => '', 'error' => "query error");
+
+    if ($data['event'] == 'select') {
+        $result = $link->query($data['sql']);
+        if (!$result)
+        {
+            return array("data" => '', 'error' => "query error");
+        }
+        $data = $result->fetchAll();
     }
-    $data = $result->fetchAll();
+
+    if ($data['event'] == 'insert' || $data['event'] == 'update' || $data['event'] == 'delete') {
+        //$data = $data['sql'];
+        $link->exec($data['sql']);
+    }
+
+    if ($data['event'] == 'beginTransaction') {
+        $link->beginTransaction();
+    }
+
+    if ($data['event'] == 'rollBack') {
+        $link->rollBack();
+    }
+
+    if ($data['event'] == 'commit') {
+        $link->commit();
+    }
+
 
     $mem = new Memcached();
     //链接一台memcahe服务
     $mem->addServer('127.0.0.1','11211');
     $fdd = $mem->get('fdd');
-    $serv->send($fdd, json_encode($data));
+    $serv->send($fdd, $serv->worker_id - $serv->setting['worker_num']);
+    $data['worder_id'] = $mem->get('worker_id');
 
     //返回任务执行的结果
     return array("data" => $data);
--- pool_client.php
+++ pool_client.php
@@ -3,7 +3,12 @@
 //以下是客户端代码
 $client = new swoole_client(SWOOLE_SOCK_TCP, SWOOLE_SOCK_SYNC); //同步阻塞
 $client->connect('127.0.0.1', 9501);
-$client->send("show tables");
-$data = $client->recv();
+$client->send(serialize(['sql' => '', 'event' => 'beginTransaction']));
+$data[1] = $client->recv();
+$client->send(serialize(['sql' => "insert user (id,name) values (9, 'long9')", 'event' => 'insert', 'worker_id' => $data[1]]));
+$data[2] = $client->recv();
+$client->send(serialize(['sql' => "", 'event' => 'commit', 'worker_id' => $data[2]]));
+$data[3] = $client->recv();
+
 
 print_r($data);
